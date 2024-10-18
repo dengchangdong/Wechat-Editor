9b14ccd2bf948641b6bbb8f9c96f54abdcec5f81
Deng Changdong <dengchangdong@gmail.com>
2024-10-18 20:05:46 +0800

    Update AboutDialog.vue

--- src/components/CodemirrorEditor/EditorHeader/AboutDialog.vue
+++ src/components/CodemirrorEditor/EditorHeader/AboutDialog.vue
@@ -41,23 +41,7 @@ function onRedirect(url: string) {
       </DialogHeader>
       <div class="text-center">
         <h3>一款高度简洁的微信 Markdown 编辑器</h3>
-        <p>扫码关注公众号 Doocs，原创技术文章第一时间推送！</p>
-        <img
-          class="mx-auto my-5"
-          src="https://cdn-doocs.oss-cn-shenzhen.aliyuncs.com/gh/doocs/md/images/1648303220922-7e14aefa-816e-44c1-8604-ade709ca1c69.png"
-          alt="Doocs Markdown 编辑器"
-          style="width: 40%"
-        >
       </div>
-      <DialogFooter class="sm:justify-evenly">
-        <Button
-          v-for="link in links"
-          :key="link.url"
-          @click="onRedirect(link.url)"
-        >
-          {{ link.label }}
-        </Button>
-      </DialogFooter>
     </DialogContent>
   </Dialog>
 </template>
